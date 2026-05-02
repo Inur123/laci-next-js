3c8620b90125983885af1f77a2b54c05e27b0d15
zainurroziqin <zainurroziqin38@gmail.com>
2026-05-02 13:18:30 +0700

    chore: update default production baseURL for auth and auth-client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { emailOTPClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_APP_URL || "https://laci.pelajarnumagetan.or.id",
   user: {
     additionalFields: {
       role: {
